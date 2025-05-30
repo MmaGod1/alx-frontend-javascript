7c0c06390d2f4207191de1c53a17544b1068542c
MmaGod1 <gmmaduchukwu@gmail.com>
2025-05-30 15:09:29 +0100

    return an Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,5 @@
+export default function createIteratorObject(report) {
+  const allEmployees = Object.values(report.allEmployees);
+  const employeeList = allEmployees.flat();
+  return employeeList[Symbol.iterator]();
+}
